65838fa8b5ee01df43ed3dad3eadc2e2f33c842f
유인호 <yuiop1029@naver.com>
2024-02-14 15:19:29 +0900

    Initial Commit

---
+++ SmartPush/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SmartPush
+//
+//  Created by A_Mcflurry on 2/14/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
